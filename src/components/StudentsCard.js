203ed8a4c131b6d0de5cd9372dc070a5b3e0219a
Niko1s <nikola.sedlackova@centrum.cz>
2023-04-17 10:21:52 +0200

    new

--- src/components/StudentsCard.js
+++ src/components/StudentsCard.js
@@ -1,18 +1,20 @@
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Tooltip from 'react-bootstrap/Tooltip';
+import Button from 'react-bootstrap/Button';
 
 
 export const StudentsCard = ({student}) => {
   return (
     <Card bg= "primary" border= "primary" style={{ width: '18rem' }}>
-      <Card.Header>Student</Card.Header>
+      <Card.Header><b>Student</b></Card.Header>
       <ListGroup variant="flush">
-        <ListGroup.Item><b>Jméno:</b> {student.firstname}</ListGroup.Item>
+        <ListGroup.Item>Jméno: {student.firstname}</ListGroup.Item>
         <ListGroup.Item>Příjmení: {student.surname}</ListGroup.Item>
-        <ListGroup.Item>Study Group: {student.study_group}</ListGroup.Item>
-        <ListGroup.Item>id: {student.id}</ListGroup.Item>
+        <ListGroup.Item>Studijní skupina: {student.study_group}</ListGroup.Item>
+        <ListGroup.Item>Id: {student.id}</ListGroup.Item>
       </ListGroup>
+      <Button variant="info">Show</Button>{' '}
     </Card>
   );
 }
