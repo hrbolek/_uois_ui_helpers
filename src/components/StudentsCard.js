88ea558def01fcf995e1afdc0e2fc368c9bcc024
Nafreju <buresmichal98@seznam.cz>
2023-04-17 09:50:15 +0200

    bold studentscard

--- src/components/StudentsCard.js
+++ src/components/StudentsCard.js
@@ -8,7 +8,7 @@ export const StudentsCard = ({student}) => {
     <Card bg= "primary" border= "primary" style={{ width: '18rem' }}>
       <Card.Header>Student</Card.Header>
       <ListGroup variant="flush">
-        <ListGroup.Item>Jméno: {student.firstname}</ListGroup.Item>
+        <ListGroup.Item><b>Jméno:</b> {student.firstname}</ListGroup.Item>
         <ListGroup.Item>Příjmení: {student.surname}</ListGroup.Item>
         <ListGroup.Item>Study Group: {student.study_group}</ListGroup.Item>
         <ListGroup.Item>id: {student.id}</ListGroup.Item>
