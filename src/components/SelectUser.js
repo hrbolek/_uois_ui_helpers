25e06b9dac3ea8cf0703686003636fe3df4e7a62
Nafreju <buresmichal98@seznam.cz>
2023-05-23 22:12:00 +0200

    need to call hook

--- src/components/SelectUser.js
+++ src/components/SelectUser.js
@@ -1,5 +1,5 @@
 
-import React from "react"
+import React, { useEffect } from "react"
 import CreatableSele from "react-select/creatable"
 import ReactSelect from 'react-select';
 import { useDispatch, useSelector } from "react-redux";
@@ -11,17 +11,17 @@ import { TaskActions } from "utils/TaskReducers";
 export const SelectUser = (props) => {
     const users = useSelector(state => state.users)
     const tasks = useSelector(state => state.tasks)
-    const [showAnotherComponent, setShowAnotherComponent] = useState(false);
-    const [allTasks, setAllTasks] = useState(null)
+    
+    const [selectedOption, setSelectedOption] = useState(null);
 
 
-    const handleSelectChange = (selectedOption) => {
+    const handleSelectChange = (selected) => {
         const selected_user = users.filter(user => {
-            return user.id === selectedOption.value;
+            return user.id === selected.value;
           });
           const all_tasks = selected_user[0].tasks
-          setShowAnotherComponent(true)
-          setAllTasks(all_tasks)
+          console.log(all_tasks)
+          setSelectedOption(all_tasks)
       };
 
     const options = users.map(user => ({label: user.name, value: user.id}))
@@ -29,7 +29,7 @@ export const SelectUser = (props) => {
         <div>
             <ReactSelect options={options} onChange={handleSelectChange}/>
 
-            {showAnotherComponent && <MultipleCards {...allTasks}/>}
+            <MultipleCards {...selectedOption}/>
         </div>
     )
 }
