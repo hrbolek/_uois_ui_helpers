d3f708534e06531dbca9e0d031f6da4223a1994c
Niko1s <nikola.sedlackova@centrum.cz>
2023-06-01 14:49:52 +0200

    home component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { ButtonAddTask } from 'components/ButtonAddTask';
 import { TaskLoader } from 'components/TaskLoader';
 import { DateInput } from 'components/DateInput';
 import { TryItComponent } from 'components/TryItComponent';
+import { HomePage } from 'components/HomeComponent';
 /*
 function getData (id= "adde473d-5c78-4171-bf16-8e7f97bef5f9") {
   const tmp = useSelector(state => state.tasks)
@@ -21,15 +22,7 @@ function App() {
   return (
     <div className="App">
       <AppProvider>
-      <div style={{ display: 'flex'}}>
-        <LoadUserData />
-        <ButtonAddTask d={true}/>
-        <TaskLoader />
-        <DateInput></DateInput>
-      </div>
-      <DateInput />
-      <TryItComponent />
-
+          <HomePage/>
       </AppProvider>
       
     </div>
