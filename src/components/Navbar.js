9ce30ae4d1eca6f4f7dc87aa0b166378aabb7a32
Niko1s <nikola.sedlackova@centrum.cz>
2023-06-01 14:36:37 +0200

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { TryItComponent } from './TryItComponent';
 
 //https://www.geeksforgeeks.org/create-a-responsive-navbar-using-reactjs/
 //https://www.youtube.com/watch?v=DTPERO1bl7w
@@ -9,17 +10,28 @@ import Navbar from 'react-bootstrap/Navbar';
 export const Navbar= (props) =>  {
   return (
     <>
+    <Router>
+    
       <Navbar bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Presences</Navbar.Brand>
+          <Navbar.Brand href="/">Presences</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#Student">Student</Nav.Link>
+            <Nav.Link href="/">Student</Nav.Link>
             <Nav.Link href="#Učitel">Učitel</Nav.Link>
             <Nav.Link href="#Úkoly">Úkoly</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <br />
+      <Routes>
+      <Route path = "/" element={<TryItComponent/>}/>
+      </Routes>
+
+      
+      
+      
+      
+      </Router>
+      
     </>
   );
 }
