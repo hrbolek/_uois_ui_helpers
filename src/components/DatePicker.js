7d8ff2effc0438c96f305563291a3d6ceb649ead
Niko1s <nikola.sedlackova@centrum.cz>
2023-06-07 15:55:33 +0200

    message changed

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -2,8 +2,8 @@ export const DatePicker= (props)=>{
   return (
     <form>
   <label>
-    Choose your preferred party date:
-    <input type="date" name="party" min="2017-04-01" max="2017-04-30" />
+    Datum odevzdání:
+    <input type="date" name="party" min="2017-04-01" max="2025-04-30" />
   </label>
 </form>
   )
