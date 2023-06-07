8c12f7893c746d846f5d65a543de6a4f8af8e7e7
Niko1s <nikola.sedlackova@centrum.cz>
2023-06-07 16:57:01 +0200

    minor changes

--- src/utils/comparator.js
+++ src/utils/comparator.js
@@ -1,7 +1,11 @@
-export function comparator_task(task1, task2){
-    if (task1.name == task2.name && task1.briefDesc==task2.briefDesc && task1.detailedDesc ==task2.detailedDesc && task1.reference ==task2.reference &&task1.dateOfSubmission==task2.dateOfSubmission &&task1.dateOfFulfillment ==task1.dateOfFulfillment)
+export function comparator_task(task1, task2)
+{
+    if (task1.name == task2.name && task1.briefDesc==task2.briefDesc &&
+         task1.detailedDesc ==task2.detailedDesc && task1.reference ==task2.reference &&
+         task1.dateOfSubmission==task2.dateOfSubmission &&task1.dateOfFulfillment ==task1.dateOfFulfillment)
     {
         return(true)  
     }
     else (false)
 }
+
