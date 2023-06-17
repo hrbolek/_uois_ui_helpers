939038aa13d6d4b6177502adc76236208b085e9d
Nafreju <buresmichal98@seznam.cz>
2023-06-17 18:50:15 +0200

    standalone task name input component

---
+++ src/components/Tasks/TaskNameInput.js
@@ -0,0 +1,15 @@
+
+export const TaskNameInput = ({setTaskName}) => {
+    const handleChange = (event) => {
+      setTaskName(event.target.value)
+    }
+  
+    return (
+        <form>
+          <div className="form-group">
+            <small className="form-text text-muted">Zadejte {name}</small>
+            <input type="text" className="form-control"  placeholder="jméno úkolu" onChange={handleChange}/>
+          </div>
+        </form>
+    )
+  }
