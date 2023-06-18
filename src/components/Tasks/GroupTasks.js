4f696a724cb1acf5e5567c6a8cba0feb460ef750
Nafreju <buresmichal98@seznam.cz>
2023-06-18 14:26:19 +0200

    deleted unnecessary lines

--- src/components/Tasks/GroupTasks.js
+++ src/components/Tasks/GroupTasks.js
@@ -4,22 +4,18 @@ import { TasksTable } from "./TasksTable"
 import { actions } from "utils/store"
 
 
-export const GroupTasks = ({groupId, actions}) => {
-    const users = useSelector(state => state.users)
-    const groups = useSelector(state => state.groups)
+export const GroupTasks = ({groupId, actions, groups}) => {
     useEffect(
         () => {
             //fetch memberships by groupId
-            if (groupId) {
-                actions.groupMembershipsFetch(groupId)
-            }
+            actions.groupMembershipsFetch(groupId)
         }, [groupId]
     )
     const memberships = groups[groupId]?.memberships
     return (
         <div>
             {memberships?.map(
-                (m, index) => <TasksTable key={index} userId={m.user.id} actions={actions} users={users}/>
+                (m, index) => <TasksTable key={index} userId={m.user.id} actions={actions}/>
             )}
         </div>
     )
