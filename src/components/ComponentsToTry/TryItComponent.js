29d3c040181d4837adbc573515705346ff724089
Nafreju <buresmichal98@seznam.cz>
2023-07-12 17:24:38 +0200

    unnecessary lines of comments deleted

--- src/components/ComponentsToTry/TryItComponent.js
+++ src/components/ComponentsToTry/TryItComponent.js
@@ -1,17 +1,6 @@
 import { TextInput } from "components/TextInput";
 import { useState } from "react"
-/*export const TryItComponent = () => {
-    return (
-        <div>
 
-            <label for="username">Username: (3-16 characters)</label>
-            <input name="username" type="text" value="Sasha" pattern="\w{3,16}" required/>
-        
-            <label for="pin">PIN: (4 digits)</label>
-            <input name="pin" type="password" pattern="\d{4,4}" required/>
-        </div>
-        )
-};*/
 export const TryItComponent = () => {
     const [isShown, setIsShown] = useState(false)
 
